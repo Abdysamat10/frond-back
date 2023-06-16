3c413b3c110fd9df513dfd0467d6f4fb3d9a325c
daurenabasov <daurenabasov987@gmail.com>
2023-06-16 14:34:32 +0600

    create structure files

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import AppRoutes from '../routes/AppRoutes';
+import Layout from "../layout/Layout"
+
+export const App = () => {
+    return (
+        <Layout>
+            <AppRoutes />
+        </Layout>
+    );
+};
+
